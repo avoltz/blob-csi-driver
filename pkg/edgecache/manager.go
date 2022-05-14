f219ee99c0d342ace410a82733dcd8aee9459751
Andy Voltz <anvoltz@microsoft.com>
2022-05-14 19:50:20 -0400

    feat: add initial support for edge cache via iscacheenabled param.
    When this param is specified, edge cache config and mount endpoints
    are used to create the volume.
    
    Edge cache volumes must be mounted via grpc calls, so the staged
    volumes are mounted in a subdir of the default `globalmount` dir.

---
+++ pkg/edgecache/manager.go
@@ -0,0 +1,192 @@
+package edgecache
+
+import (
+	"context"
+	"path/filepath"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/blob-csi-driver/pkg/edgecache/blob_cache_volume"
+	"sigs.k8s.io/blob-csi-driver/pkg/edgecache/cache_volume_service"
+	"sigs.k8s.io/blob-csi-driver/pkg/edgecache/csi_mounts"
+)
+
+type Manager struct {
+	connectTimeout int
+	configEndpoint string
+	mountEndpoint  string
+}
+
+func NewManager(connectTimeout int, configEndpoint string, mountEndpoint string) *Manager {
+	return &Manager{
+		connectTimeout: connectTimeout,
+		configEndpoint: configEndpoint,
+		mountEndpoint:  mountEndpoint,
+	}
+}
+
+func GetStagingPath(path string) string {
+	return filepath.Join(path, "edgecache")
+}
+
+func sendGetVolume(client cache_volume_service.CacheVolumeClient, account string, container string) (error, bool) {
+	blobVolumeName := blob_cache_volume.Name{
+		Account:   &account,
+		Container: &container,
+	}
+
+	getReq := cache_volume_service.GetBlobRequest{
+		Names: []*blob_cache_volume.Name{&blobVolumeName},
+	}
+
+	klog.V(2).Infof("calling edge cache GetBlob")
+	getRsp, err := client.GetBlob(context.TODO(), &getReq)
+	if err != nil {
+		klog.Errorf("GRPC call returned with an error: %v", err)
+		return err, false
+	}
+	klog.V(2).Infof("GetBlob found %d volumes", len(getRsp.Volumes))
+	found := len(getRsp.Volumes) > 0
+	return err, found
+}
+
+func sendCreateVolume(client cache_volume_service.CacheVolumeClient, account string, container string, key string) error {
+	authenticator := blob_cache_volume.Authenticator{
+		Authenticator: &blob_cache_volume.Authenticator_AccountKey{AccountKey: key},
+	}
+
+	blobVolume := blob_cache_volume.BlobCacheVolume{
+		Name: &blob_cache_volume.Name{
+			Account:   &account,
+			Container: &container,
+		},
+		Auth: &authenticator,
+	}
+
+	addReq := cache_volume_service.CreateBlobRequest{
+		Volume: &blobVolume,
+	}
+
+	klog.V(2).Infof("calling edge cache CreateBlob")
+	_, err := client.CreateBlob(context.TODO(), &addReq)
+	if err != nil {
+		klog.Errorf("GRPC call returned with an error: %v", err)
+		return err
+	}
+	klog.V(2).Infof("CreateBlob succeeded")
+	return nil
+}
+
+func sendMount(client csi_mounts.CSIMountsClient, account string, container string, target_path string, interval time.Duration, timeout time.Duration) error {
+	blobVolume := blob_cache_volume.Name{
+		Account:   &account,
+		Container: &container,
+	}
+	addReq := csi_mounts.AddMountReq{
+		TargetPath: &target_path,
+		VolumeInfo: &csi_mounts.VolumeInfo{
+			VolumeInfo: &csi_mounts.VolumeInfo_BlobVolume{
+				BlobVolume: &blobVolume,
+			},
+		},
+	}
+
+	mntctx, mntcancel := context.WithCancel(context.Background())
+	defer mntcancel()
+
+	// There can be a delay between CreateBlob and the mount being available to the Mount Service
+	// Use a goroutine to try a few times
+	result := make(chan bool)
+	klog.V(2).Infof("Edgecache AddMount: %s", &addReq)
+	go func() {
+		for {
+			_, err := client.AddMount(context.TODO(), &addReq)
+			if err != nil {
+				klog.Warningf("AddMount GRPC failed (will retry) returned with an error: %v", err)
+				time.Sleep(interval)
+			} else {
+				result <- true
+				return
+			}
+			if mntctx.Err() != nil {
+				klog.Errorf("AddMount cancelled %q", mntctx.Err().Error())
+				return
+			}
+		}
+	}()
+	select {
+	case <-result:
+		klog.V(2).Infof("edge cache AddMount success")
+	case <-time.After(timeout):
+		return status.Errorf(codes.DeadlineExceeded, "Deadline exceeded for mount %q", target_path)
+	}
+	return nil
+}
+
+func sendUnmount(client csi_mounts.CSIMountsClient, target_path string) error {
+	rmReq := csi_mounts.RemoveMountReq{
+		TargetPath: &target_path,
+	}
+
+	klog.V(2).Infof("Calling RemoveMount: %s", &rmReq)
+	if _, err := client.RemoveMount(context.TODO(), &rmReq); err != nil {
+		klog.Errorf("GRPC call returned with an error: %v", err)
+		return err
+	}
+	return nil
+}
+
+func createVolume(client cache_volume_service.CacheVolumeClient, accountName string, accountKey string, containerName string) error {
+	if err, found := sendGetVolume(client, accountName, containerName); err != nil {
+		return err
+	} else if !found {
+		klog.V(2).Infof("volume does not exist, creating...")
+		return sendCreateVolume(client, accountName, containerName, accountKey)
+	}
+	return nil
+}
+
+func (m *Manager) EnsureVolume(accountName string, accountKey string, containerName string, targetPath string) error {
+	connectionTimeout := time.Duration(m.connectTimeout) * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), connectionTimeout)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, m.configEndpoint, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	client := cache_volume_service.NewCacheVolumeClient(conn)
+	return createVolume(client, accountName, accountKey, containerName)
+}
+
+func (m *Manager) MountVolume(account string, container string, target_path string) error {
+	connectionTimeout := time.Duration(m.connectTimeout) * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), connectionTimeout)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, m.mountEndpoint, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	client := csi_mounts.NewCSIMountsClient(conn)
+	return sendMount(client, account, container, target_path, 500*time.Millisecond, 5*time.Second)
+}
+
+func (m *Manager) UnmountVolume(volumeID string, targetPath string) error {
+	connectionTimeout := time.Duration(m.connectTimeout) * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), connectionTimeout)
+	defer cancel()
+	conn, err := grpc.DialContext(ctx, m.mountEndpoint, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	client := csi_mounts.NewCSIMountsClient(conn)
+	return sendUnmount(client, targetPath)
+}
---
+++ pkg/edgecache/manager_test.go
@@ -0,0 +1,143 @@
+package edgecache
+
+import (
+	"flag"
+	"io"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"k8s.io/klog/v2"
+	"sigs.k8s.io/blob-csi-driver/pkg/edgecache/blob_cache_volume"
+	"sigs.k8s.io/blob-csi-driver/pkg/edgecache/cache_volume_service"
+	"sigs.k8s.io/blob-csi-driver/pkg/edgecache/csi_mounts"
+	"sigs.k8s.io/blob-csi-driver/pkg/edgecache/mock_cache_volume_service"
+	"sigs.k8s.io/blob-csi-driver/pkg/edgecache/mock_csi_mounts"
+)
+
+func NewFakeManager() *Manager {
+	return NewManager(5, "", "")
+}
+
+func TestNewFakeManager(t *testing.T) {
+	m := NewFakeManager()
+	assert.NotNil(t, m)
+}
+
+func TestCreateVolume(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	client := mock_cache_volume_service.NewMockCacheVolumeClient(ctrl)
+	account := "account"
+	container := "container"
+	key := "key"
+	name := blob_cache_volume.Name{
+		Account:   &account,
+		Container: &container,
+	}
+	getReq := cache_volume_service.GetBlobRequest{
+		Names: []*blob_cache_volume.Name{&name},
+	}
+	rspVolume := blob_cache_volume.BlobCacheVolume{Name: &name}
+	authenticator := blob_cache_volume.Authenticator{
+		Authenticator: &blob_cache_volume.Authenticator_AccountKey{AccountKey: key},
+	}
+	createReq := cache_volume_service.CreateBlobRequest{
+		Volume: &blob_cache_volume.BlobCacheVolume{
+			Name: &name,
+			Auth: &authenticator,
+		},
+	}
+	createRsp := cache_volume_service.CreateBlobResponse{}
+	t.Run("NoBlobFoundWillCreate", func(t *testing.T) {
+		getRsp := cache_volume_service.GetBlobResponse{}
+		client.EXPECT().GetBlob(gomock.Any(), &getReq).Times(1).Return(&getRsp, nil)
+		client.EXPECT().CreateBlob(gomock.Any(), &createReq).Times(1).Return(&createRsp, nil)
+		err := createVolume(client, account, key, container)
+		assert.Nil(t, err)
+
+	})
+	t.Run("BlobFoundDoesNotCreate", func(t *testing.T) {
+		getRsp := cache_volume_service.GetBlobResponse{
+			Volumes: []*blob_cache_volume.BlobCacheVolume{&rspVolume},
+		}
+		client.EXPECT().GetBlob(gomock.Any(), &getReq).Times(1).Return(&getRsp, nil)
+		client.EXPECT().CreateBlob(gomock.Any(), gomock.Any()).Times(0)
+		err := createVolume(client, account, key, container)
+		assert.Nil(t, err)
+	})
+}
+
+func TestSendMount(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	client := mock_csi_mounts.NewMockCSIMountsClient(ctrl)
+	successRsp := csi_mounts.AddMountRsp{}
+	targetPath := "target/path"
+	account := "account"
+	container := "container"
+	vol := blob_cache_volume.Name{
+		Account:   &account,
+		Container: &container,
+	}
+	addReq := csi_mounts.AddMountReq{
+		TargetPath: &targetPath,
+		VolumeInfo: &csi_mounts.VolumeInfo{
+			VolumeInfo: &csi_mounts.VolumeInfo_BlobVolume{
+				BlobVolume: &vol,
+			},
+		},
+	}
+	interval := time.Duration(1 * time.Microsecond)
+	timeout := time.Duration(10 * time.Millisecond)
+	t.Run("WorksFirstTry", func(t *testing.T) {
+		client.EXPECT().AddMount(gomock.Any(), &addReq).Times(1).Return(&successRsp, nil)
+		ret := sendMount(client, account, container, targetPath, interval, timeout)
+		assert.Nil(t, ret)
+	})
+	t.Run("WorksWithRetries", func(t *testing.T) {
+		gomock.InOrder(
+			client.EXPECT().AddMount(gomock.Any(), &addReq).Return(nil, status.Errorf(codes.Internal, "")),
+			client.EXPECT().AddMount(gomock.Any(), &addReq).Return(&successRsp, nil),
+		)
+		ret := sendMount(client, account, container, targetPath, interval, timeout)
+		assert.Nil(t, ret)
+	})
+	t.Run("CancelsEventually", func(t *testing.T) {
+		client.EXPECT().AddMount(gomock.Any(), &addReq).MinTimes(1).Return(nil, status.Errorf(codes.DeadlineExceeded, ""))
+		ret := sendMount(client, account, container, targetPath, interval, timeout)
+		assert.NotNil(t, ret)
+	})
+}
+
+func TestSendUnmount(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	client := mock_csi_mounts.NewMockCSIMountsClient(ctrl)
+	targetPath := "target/path"
+	rmReq := csi_mounts.RemoveMountReq{
+		TargetPath: &targetPath,
+	}
+	rmRsp := csi_mounts.RemoveMountRsp{}
+	t.Run("Success", func(t *testing.T) {
+		client.EXPECT().RemoveMount(gomock.Any(), &rmReq).Times(1).Return(&rmRsp, nil)
+		ret := sendUnmount(client, targetPath)
+		assert.Nil(t, ret)
+	})
+	t.Run("Fail", func(t *testing.T) {
+		err := status.Errorf(codes.Internal, "")
+		client.EXPECT().RemoveMount(gomock.Any(), &rmReq).Times(1).Return(nil, err)
+		ret := sendUnmount(client, targetPath)
+		assert.Equal(t, ret, err)
+	})
+}
+
+func TestMain(m *testing.M) {
+	klog.InitFlags(nil)
+	flag.Set("logtostderr", "false")
+	flag.Set("alsologtostderr", "false")
+	flag.Set("stderrthreshold", "10")
+	klog.SetOutput(io.Discard)
+	os.Exit(m.Run())
+}
