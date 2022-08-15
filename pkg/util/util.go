69ea2f96755bb2c9f60026004c4751c2ce409a9b
Andy Voltz <anvoltz@microsoft.com>
2022-08-15 17:10:37 -0400

    chore[util]: copy in functions and their tests from kubernetes core

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -177,3 +177,36 @@ func GetKubeClient(kubeconfig string) (*kubernetes.Clientset, error) {
 
 	return kubernetes.NewForConfig(config)
 }
+
+// Function copied from kubernetes/pkg/util/slice/slice.go
+func ContainsString(slice []string, s string, modifier func(s string) string) bool {
+	for _, item := range slice {
+		if item == s {
+			return true
+		}
+		if modifier != nil && modifier(item) == s {
+			return true
+		}
+	}
+	return false
+}
+
+// Function copied from kubernetes/pkg/util/slice/slice.go
+func RemoveString(slice []string, s string, modifier func(s string) string) []string {
+	newSlice := make([]string, 0)
+	for _, item := range slice {
+		if item == s {
+			continue
+		}
+		if modifier != nil && modifier(item) == s {
+			continue
+		}
+		newSlice = append(newSlice, item)
+	}
+	if len(newSlice) == 0 {
+		// Sanitize for unit tests so we don't need to distinguish empty array
+		// and nil.
+		newSlice = nil
+	}
+	return newSlice
+}
